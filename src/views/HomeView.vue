4e240706daf3d1a014fc94b2e928ddd00c065ee3
duyhh <duyhh@irender.vn>
2022-11-08 20:18:08 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,23 +57,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="flex-fill d-flex flex-column m-1 mr-4 p-2 rounded" style="border: 2px solid #e0e0e0; background-color: #f0f0f0;">
-				<div class="text-center mb-3">DATA ACCESS PROVIDER</div>
-				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-				</div>
-			</div>
 			<div class="flex-fill d-flex flex-column m-1 mr-0 p-2 rounded" style="border: 2px solid #e0e0e0; background-color: #f0f0f0;">
 				<div class="text-center mb-3">ENDPOINT DATA</div>
 				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
