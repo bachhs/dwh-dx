e4b42abbda6d112c53405ccde42f8f2d6566a3a9
duyhh <duyhh@irender.vn>
2022-11-08 20:22:21 +0700

    update UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,16 +9,16 @@
 			<div class="flex-fill d-flex flex-column m-1 mr-4 ml-0 p-2 rounded" style="border: 2px solid #e0e0e0; height: calc(100vh - 7.4rem); background-color: #f0f0f0;">
 				<div class="text-center mb-3">NGUỒN DỮ LIỆU</div>
 				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
+					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3 rounded" style="border: 2px solid #e0e0e0; background-color: #fff;">
 						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
 					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
+					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3 rounded" style="border: 2px solid #e0e0e0; background-color: #fff;">
 						<img src="/custom-img/svg-icon/database-icon.svg" style="width: 8rem;" />
 					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
+					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3 rounded" style="border: 2px solid #e0e0e0; background-color: #fff;">
 						<img src="/custom-img/svg-icon/file-icon.svg" style="width: 8rem;" />
 					</div>
-					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
+					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3 rounded" style="border: 2px solid #e0e0e0; background-color: #fff;">
 						<img src="/custom-img/svg-icon/rest-api.svg" style="width: 8rem;" />
 					</div>
 				</div>
@@ -26,52 +26,19 @@
 			<div class="flex-fill d-flex flex-column m-1 mr-4 p-2 rounded" style="border: 2px solid #e0e0e0; background-color: #f0f0f0;">
 				<div class="text-center mb-3">XỮ LÝ DỮ LIỆU</div>
 				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/database-icon.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
+					
 				</div>
 			</div>
 			<div class="flex-fill d-flex flex-column m-1  mr-4 p-2 rounded" style="border: 2px solid #e0e0e0; background-color: #f0f0f0;">
 				<div class="text-center mb-3">DATA WARE HOUSE</div>
 				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
+					
 				</div>
 			</div>
 			<div class="flex-fill d-flex flex-column m-1 mr-0 p-2 rounded" style="border: 2px solid #e0e0e0; background-color: #f0f0f0;">
 				<div class="text-center mb-3">ENDPOINT DATA</div>
 				<div class="text-center flex-fill d-flex flex-column pl-2 pr-2">
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-4 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
-					<div class="mb-2 flex-fill d-flex align-items-center justify-content-center pt-3 pb-3" style="border: 2px solid #e0e0e0; background-color: #fff;">
-						<img src="/custom-img/svg-icon/ms-excel.svg" style="width: 8rem;" />
-					</div>
+					
 				</div>
 			</div>
 		</div>
