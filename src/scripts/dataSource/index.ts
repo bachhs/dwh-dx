f48a94f011c0cbbc6b4312a58fb53686ff6d4d96
Minh Dao <daoducminh1997@gmail.com>
2022-11-14 23:04:08 +0700

    Add get all datasources

--- src/scripts/dataSource/index.ts
+++ src/scripts/dataSource/index.ts
@@ -1,33 +1,46 @@
-import { reactive, ref } from 'vue';
+import { reactive, ref } from "vue";
+import axios from "axios";
+
+const BASE_URL = "http://14.225.11.182:8081/api/v1";
+
 export default {
     setup() {
         const data = reactive({
-            sourceData: ref(''),
-            typeDataFormat: ref(''),
-            filterQuery: ref(''),
-            options : [
+            datasources: ref([]),
+            typeDataFormat: ref(""),
+            filterQuery: ref(""),
+            options: [
                 {
-                    value: 'Option1',
-                    label: 'Option1',
+                    value: "Option1",
+                    label: "Option1",
                 },
                 {
-                    value: 'Option2',
-                    label: 'Option2',
+                    value: "Option2",
+                    label: "Option2",
                 },
                 {
-                    value: 'Option3',
-                    label: 'Option3',
+                    value: "Option3",
+                    label: "Option3",
                 },
                 {
-                    value: 'Option4',
-                    label: 'Option4',
+                    value: "Option4",
+                    label: "Option4",
                 },
                 {
-                    value: 'Option5',
-                    label: 'Option5',
+                    value: "Option5",
+                    label: "Option5",
                 },
-            ]
+            ],
         });
         return data;
     },
-}
+    methods: {
+        async getAllDataSources() {
+            const response = await axios.get(`${BASE_URL}/datasources`, { page: 1, limit: 20 });
+            this.datasources = response.data.content;
+        }
+    },
+    mounted() {
+        this.getAllDataSources();
+    }
+};
--- src/views/dataSource/Index.vue
+++ src/views/dataSource/Index.vue
@@ -1,40 +1,30 @@
-<script lang="ts" src="@/scripts/dataSource">
-
-</script>
+<script lang="ts" src="@/scripts/dataSource"></script>
 <template>
-    <el-card class="box-card w-100 d-flex flex-fill flex-column"
-		:body-style="{ padding: '0.8rem 1rem' }">
+    <el-card class="box-card w-100 d-flex flex-fill flex-column" :body-style="{ padding: '0.8rem 1rem' }">
         <div class="flex-fill d-flex flex-column w-100">
             <div class="d-flex align-items-center">
                 <div class="flex-fill d-none d-md-block">
-                    <h4><i class="fas fa-database text-navy"></i> <strong>Nguồn dữ liệu</strong></h4>
+                    <h4>
+                        <i class="fas fa-database text-navy"></i>
+                        <strong>Nguồn dữ liệu</strong>
+                    </h4>
                 </div>
                 <div class="d-flex align-items-center">
                     <div class="ml-1 mr-1">
-                        <el-select v-model="sourceData" filterable placeholder="Nguồn dữ liệu.."
-                            size="large">
-                            <el-option
-                            v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value"
-                            />
+                        <el-select v-model="sourceData" filterable placeholder="Nguồn dữ liệu.." size="large">
+                            <el-option v-for="item in options" :key="item.value" :label="item.label"
+                                :value="item.value" />
                         </el-select>
                     </div>
                     <div class="ml-1 mr-1">
-                        <el-select v-model="typeDataFormat" filterable placeholder="Loại dữ liệu.."
-                            size="large">
-                            <el-option
-                            v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value"
-                            />
+                        <el-select v-model="typeDataFormat" filterable placeholder="Loại dữ liệu.." size="large">
+                            <el-option v-for="item in options" :key="item.value" :label="item.label"
+                                :value="item.value" />
                         </el-select>
                     </div>
                     <div class="ml-1 mr-1">
-                        <el-input v-model="filterQuery" filterable placeholder="Nhập để lọc dữ liệu.."
-                            size="large" style="min-width: 16rem;">
+                        <el-input v-model="filterQuery" filterable placeholder="Nhập để lọc dữ liệu.." size="large"
+                            style="min-width: 16rem">
                             <template #prepend>
                                 <el-icon style="vertical-align: middle">
                                     <Search />
@@ -49,7 +39,7 @@
                             </el-icon>
                         </el-button>
 
-                        <el-button size="large" type="primary">
+                        <el-button size="large" type="primary" @click="getAllDataSources()">
                             <el-icon :size="20" style="vertical-align: middle">
                                 <Refresh />
                             </el-icon>
@@ -61,22 +51,25 @@
                 <el-scrollbar class="w-100 flex-fill">
                     <div class="mt-2 mr-3">
                         <div class="row">
-                            <div class="col-12 col-sm-6 col-md-4 mb-3"
-                                 v-for="(itemDemo) in 99" :key="itemDemo">
+                            <div class="col-12 col-sm-6 col-md-4 mb-3" v-for="ds in datasources" :key="ds.id">
                                 <el-card :body-style="{ padding: '0.5rem 0.8rem' }">
                                     <div class="d-flex align-items-center">
                                         <div>
-                                            <img src="/custom-img/postgre.png" style="width: 5rem;" />
+                                            <img src="/custom-img/postgre.png" style="width: 5rem" />
                                         </div>
                                         <div class="ml-2">
-                                            <div><strong class="text-navy">Demo data source {{ itemDemo }}</strong></div>
-                                            <div>Loại: Postgre</div>
-                                            <div class="text-muted" style="font-size: 85%;"><span>Chưa có mô tả</span></div>
-                                            <div class="text-muted" style="font-size: 85%;">
-                                                Nguồn: Sở Y Tế
+                                            <div>
+                                                <strong class="text-navy">{{ ds.name }}</strong>
+                                            </div>
+                                            <div>Loại: {{ ds.dialect }}</div>
+                                            <div class="text-muted" style="font-size: 85%">
+                                                <span>Chưa có mô tả</span>
+                                            </div>
+                                            <div class="text-muted" style="font-size: 85%">
+                                                Nguồn: {{ ds.organization }}
                                             </div>
                                         </div>
-                                    </div>                                    
+                                    </div>
                                 </el-card>
                             </div>
                         </div>
@@ -90,7 +83,8 @@
                         <span><strong>Last update:</strong> 2 seconds ago</span>
                     </div>
                     <div>
-                        <el-pagination class="" background layout="total, sizes, prev, pager, next, jumper" :total="1000" />
+                        <el-pagination class="" background layout="total, sizes, prev, pager, next, jumper"
+                            :total="1000" />
                     </div>
                 </div>
             </div>
