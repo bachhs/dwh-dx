6683c31b5631a26623fbe11e696933a8c2a278fd
bach.luu <bach.luu@jmango360.com>
2022-11-15 15:19:17 +0700

    composition api

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import ElementPlus from 'element-plus';
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-import 'element-plus/dist/index.css';
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "element-plus/dist/index.css";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+
+const axiosInstance = axios.create();
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+  app.component(key, component);
 }
+
+app.config.globalProperties.axios = axiosInstance;
+app.provide("axios", axiosInstance);
 
 app.mount("#vue-app");
--- src/scripts/dataSource/index.ts
+++ src/scripts/dataSource/index.ts
@@ -4,43 +4,45 @@ import axios from "axios";
 const BASE_URL = "http://14.225.11.182:8081/api/v1";
 
 export default {
-    setup() {
-        const data = reactive({
-            datasources: ref([]),
-            typeDataFormat: ref(""),
-            filterQuery: ref(""),
-            options: [
-                {
-                    value: "Option1",
-                    label: "Option1",
-                },
-                {
-                    value: "Option2",
-                    label: "Option2",
-                },
-                {
-                    value: "Option3",
-                    label: "Option3",
-                },
-                {
-                    value: "Option4",
-                    label: "Option4",
-                },
-                {
-                    value: "Option5",
-                    label: "Option5",
-                },
-            ],
-        });
-        return data;
+  setup() {
+    const data = reactive({
+      datasources: ref([]),
+      typeDataFormat: ref(""),
+      filterQuery: ref(""),
+      options: [
+        {
+          value: "Option1",
+          label: "Option1",
+        },
+        {
+          value: "Option2",
+          label: "Option2",
+        },
+        {
+          value: "Option3",
+          label: "Option3",
+        },
+        {
+          value: "Option4",
+          label: "Option4",
+        },
+        {
+          value: "Option5",
+          label: "Option5",
+        },
+      ],
+    });
+    return data;
+  },
+  methods: {
+    async getAllDataSources() {
+      const response = await axios.get(`${BASE_URL}/datasources`, {
+        params: { page: 1, limit: 20 },
+      });
+      this.datasources = response.data.content;
     },
-    methods: {
-        async getAllDataSources() {
-            const response = await axios.get(`${BASE_URL}/datasources`, { page: 1, limit: 20 });
-            this.datasources = response.data.content;
-        }
-    },
-    mounted() {
-        this.getAllDataSources();
-    }
+  },
+  mounted() {
+    this.getAllDataSources();
+  },
 };
