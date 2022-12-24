6e0a333592538118b4cb7bf3ab409b23f479b9a4
Minh Dao <minh.dao@jmango360.com>
2022-12-24 17:18:41 +0700

    Ađ tag api

--- src/api/tagApi.ts
+++ src/api/tagApi.ts
@@ -1,7 +1,30 @@
-import type { ListTagResponse } from '@/types/tag';
+import type { AddTagCategoryRequest, ListTagResponse } from '@/types/tag';
 import axios from 'axios';
+
 export const tagApi = {
     tagList() {
         return axios.get<ListTagResponse>(`/meta/tag`);
+    },
+    createTagCategory(request: AddTagCategoryRequest) {
+        return axios.post(`/meta/tag`, request);
+    },
+    updateTagCategory(tabCategoryName: string, request: AddTagCategoryRequest) {
+        return axios.put(`/meta/tag/${tabCategoryName}`, request);
+    },
+    deleteTagCategory(tagCategoryName: string) {
+        return axios.delete(`/meta/tag/${tagCategoryName}`);
+    },
+    createTag(tagCategoryName: string, request: AddTagCategoryRequest) {
+        return axios.post(`/meta/tag/${tagCategoryName}`, request);
+    },
+    updateTag(
+        tagCategoryName: string,
+        tagName: string,
+        request: AddTagCategoryRequest
+    ) {
+        return axios.put(`/meta/tag/${tagCategoryName}/${tagName}`, request);
+    },
+    deleteTag(tagCategoryName: string, tagName: string) {
+        return axios.delete(`/meta/tag/${tagCategoryName}/${tagName}`);
     },
 };
