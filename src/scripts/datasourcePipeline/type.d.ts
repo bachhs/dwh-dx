eda833c0ffebd88f6c98ba3c5c9704ec3a13e378
Minh Dao <minh.dao@jmango360.com>
2022-12-24 14:05:47 +0700

    Update

--- src/scripts/datasourcePipeline/type.d.ts
+++ src/scripts/datasourcePipeline/type.d.ts
@@ -24,7 +24,7 @@ export interface MetaPipeline {
     pipelineType: string;
     service: MetaService;
     sourceConfig: SourceConfig;
-    pipelineStatus: PipelineStatus;
+    pipelineStatus: PipelineStatus[];
 }
 
 export interface ListPipelineResponse {
