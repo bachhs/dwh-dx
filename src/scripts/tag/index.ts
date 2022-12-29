211894bd3a2a0f6e0c19d54f78ecd77893eef973
duyhh <duyhh@irender.vn>
2022-12-30 06:12:05 +0700

    update tags

--- src/scripts/tag/index.ts
+++ src/scripts/tag/index.ts
@@ -1,14 +1,26 @@
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import SetDescriptionModal from '@/components/modals/SetDescriptionModal.vue';
+import type { AddTagCategoryRequest, AddTagRequest, ListTagResponse, } from '@/types/tag';
+import { tagApi } from "@/api/tagApi";
 export default {
     components: {
         SetDescriptionModal,
     },
     setup() {
-        const tagSelected = ref<number>(1);
-
+        const tagSelected = ref<any>();
+        const tagItemData = ref<ListTagResponse>();
+        const getTagList = () => {
+            tagApi.tagList().then((response:any) =>{
+                tagItemData.value = response.data;
+                if(tagItemData.value.data && tagItemData.value.data.length > 0) tagSelected.value = tagItemData.value.data[0];
+            });
+        }
+        onMounted(() =>{
+            getTagList();
+        });
         return {
             tagSelected,
+            tagItemData,
         };
     },
 };
--- src/views/tag/index.vue
+++ src/views/tag/index.vue
@@ -15,20 +15,22 @@
                         </div>
                     </div>
                     <div class="mt-2 tag-list-container">
+                        <!-- {{ tagItemData }} -->
                         <div class="card ml-3 mr-3">
                             <div class="card-body p-0">
-                                <ul class="nav nav-pills flex-column">
+                                <ul class="nav nav-pills flex-column"
+                                    v-if="tagItemData && tagItemData.data && tagItemData.data.length > 0">
                                     <li class="nav-item"
-                                        v-bind:class="{ 'tag-selected': item === tagSelected }"
-                                        v-for="item in 10" :key="item">
+                                        v-bind:class="{ 'tag-selected': tagItem.id === tagSelected.id }"
+                                        v-for="tagItem in tagItemData.data" :key="tagItem.id">
                                         <a href="javascript:void(0);" class="nav-link p-0"
-                                            @click="tagSelected = item">
+                                            @click="tagSelected = tagItem">
                                             <div class="tag-item d-flex align-items-center pt-2 pb-2 ml-2 mr-2">
                                                 <div class="flex-fill tag-label">
-                                                    PersonalData
+                                                    {{tagItem.name}}
                                                 </div>
                                                 <div class="bage-count badge">
-                                                    5
+                                                    {{tagItem.children ? tagItem.children.length: 0}}
                                                 </div>
                                             </div>
                                         </a>
@@ -43,27 +45,96 @@
             </div>
             <div class="col-12 col-md-10 d-flex flex-column">
                 <el-card class="flex-fill" :body-style="{ padding: '0px' }">
-                    <div class="p-2 pl-3 pr-3">
+                    <div class="p-2 pl-3 pr-3 d-flex flex-column" 
+                        v-if="tagSelected" 
+                        :style="`height: calc(100vh - 7rem);`">
                         <div class="d-flex">
-                            <div class="flex-fill">Management for tag <strong class="text-navy">PersonalData</strong></div>
+                            <div class="flex-fill">
+                                <h4><strong class="text-navy">{{tagSelected.name}}</strong></h4>
+                            </div>
                             <div>
                                 <el-button type="primary">Add new tag</el-button>
-                                <el-button type="primary">Delete category</el-button>
+                                <el-button type="danger">Delete category</el-button>
                             </div>
                         </div>
-                        <div class="mt-3">
-                            <p>Cụ thể, giá nhập từ nhà phân phối của sản phẩm này rơi vào khoảng 22 triệu đồng. Tuy nhiên, hiện tại trên thị trường giá của MacBook Air M1 đang dao động 20-23,5 triệu đồng. Có thể thấy tại nhiều chuỗi bán lẻ, giá bán ra của mẫu máy này đang thấp hơn cả giá nhập vào.</p>
-                            <SetDescriptionModal>
-                                <template #label>
-                                    <div class="m-2 d-flex align-items-center">
-                                        <el-icon :size="20">
-                                            <Edit />
-                                        </el-icon>
-                                        <span class="ml-2">Edit</span>
-                                    </div>
-                                </template>
-                            </SetDescriptionModal>
+                        <div class="mt-2 card p-3 pt-0 pb-0">
+                            <div class="d-flex">
+                                <div class="flex-fill">
+                                    <div v-html="tagSelected.description"></div>
+                                </div>
+                                <div class="ml-3">
+                                    <SetDescriptionModal v-model="tagSelected.description">
+                                        <template #label>
+                                            <div class="m-2 d-flex align-items-center">
+                                                <el-icon :size="20">
+                                                    <Edit />
+                                                </el-icon>
+                                                <span class="ml-0"></span>
+                                            </div>
+                                        </template>
+                                    </SetDescriptionModal>
+                                </div>
+                            </div>
                         </div>
+                        <el-scrollbar class="flex-fill" >
+                            <div class="mr-3">
+                                <table v-if="tagSelected && tagSelected.children && tagSelected.children.length > 0"
+                                    class="w-100 table table-borderless table-customize table-head-fixed text-nowrap table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 1%;" class="text-nowrap">Name</th>
+                                            <th class="text-left">Description</th>
+                                            <th style="width: 1%;" class="text-nowrap">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="tagChildItem in tagSelected.children" v-bind:key="tagChildItem.id">
+                                            <td class="text-nowrap">
+                                                <div class="pr-3">{{tagChildItem.name}}</div>
+                                            </td>
+                                            <td>
+                                                <div class="w-100 d-flex align-items-center">
+                                                    <div v-if="tagChildItem.description">
+                                                        <div class="text-normal" style="white-space: normal;" v-html="tagChildItem.description"></div>
+                                                    </div>
+                                                    <div v-else>No description</div>
+                                                    <!-- <SetDescriptionModal v-model="tagChildItem.description">
+                                                        <template #label>
+                                                            <span class="m-2 d-flex align-items-center">
+                                                                <el-icon :size="20">
+                                                                    <Edit />
+                                                                </el-icon>
+                                                            </span>
+                                                        </template>
+                                                    </SetDescriptionModal> -->
+                                                </div>   
+                                            </td>
+                                            <td class="text-center">
+                                                <el-tooltip
+                                                    class="box-item text-nowrap"
+                                                    effect="dark"
+                                                    content="Xoá"
+                                                    placement="left" >
+                                                    <el-button
+                                                        size="large"
+                                                        link
+                                                        class="text-danger">
+                                                        <el-icon
+                                                            :size="20"
+                                                            style="vertical-align: middle;" >
+                                                            <Delete />
+                                                        </el-icon>
+                                                    </el-button>
+                                                </el-tooltip>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>                                
+                                <div v-else>
+                                    <no-data class="mt-2 mt-md-5"/>
+                                </div>
+                            </div>
+                        </el-scrollbar>
                     </div>
                 </el-card>
             </div>
@@ -89,7 +160,7 @@
         .bage-count{
             background-color: #d0d0d0 !important;
             font-size: 0.8rem;
-            color: #09345d;
+            color: #707070;
         }
         &.tag-selected{
             color: #007bff;
@@ -101,6 +172,33 @@
             .bage-count{
                 background-color: #09345d !important;
                 color: #fff;
+            }
+        }
+    }
+}
+
+.table-customize {
+    thead {
+        tr {
+            th {
+                box-shadow: none !important;
+                background-color: #d3eaff !important;
+                border: none !important;
+                &:before {
+                    display: none !important;
+                }
+            }
+        }
+    }
+    tbody {
+        tr {
+            &:nth-of-type(even) {
+                background-color: #e5e5e5;
+            }
+            .link-schema {
+                &:hover {
+                    text-decoration: underline;
+                }
             }
         }
     }
