88620a998db9b89ea4a3f993825b294d8753a240
bachhs <bachhs21@gmail.com>
2023-01-07 15:35:54 +0700

    add loading handler on nifi

--- src/views/dataFlow/Index.vue
+++ src/views/dataFlow/Index.vue
@@ -1,6 +1,9 @@
+<script lang="ts" src="@/scripts/dataFlow"></script>
 <template>
     <iframe
+        id="nifi-iframe"
         src="https://nifi.dtcsolution.vn/nifi/"
         height="100%"
-        frameborder="0"></iframe>
+        frameborder="0"
+        v-on:load="load"></iframe>
 </template>
