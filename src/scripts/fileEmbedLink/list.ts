3b2180c14d2d8a6a9e7619fdd2c701978b3e67b9
duyhh <duyhh@irender.vn>
2023-02-03 13:43:59 +0700

    update file

--- src/scripts/fileEmbedLink/list.ts
+++ src/scripts/fileEmbedLink/list.ts
@@ -4,6 +4,7 @@ import { fileEmbedLinkApi } from "@/api/fileEmbedLinkApi";
 import { useDataCategoryStore } from "@/stores/dataCategory";
 import { useRoute, useRouter } from "vue-router";
 import usePaginationList from "@/scripts/_baseScripts/_usePaginationList";
+import { ElMessage, ElMessageBox } from 'element-plus';
 export default {
     props: ["viewSettings"],
     emits: ["onChangeView"],
@@ -25,11 +26,40 @@ export default {
         } = usePaginationList(fileEmbedLinkApi.dataItemList, filterDataInfo);
 
         const deleteItem = (item: any) => {
-            deleteElement(
-                fileEmbedLinkApi.deleteItem(item.id),
-                moduleName,
-                item.id
-            );
+            // deleteElement(
+            //     fileEmbedLinkApi.deleteItem(item.id),
+            //     moduleName,
+            //     item.id
+            // );
+            let showErrorMsg =(msg:string) =>{
+                ElMessage({
+                    dangerouslyUseHTMLString: true,
+                    type: 'info',
+                    message: `Đã có lỗi xảy ra khi xoá ${moduleName} <strong class="text-primary">${item.id}</strong>. ${msg}`,
+                })
+            };
+            ElMessageBox.confirm(
+                `Đồng ý sẽ xoá ${moduleName} <strong class="text-primary">${item.id}</strong>. Tiếp tục?`, 'Xác nhận xoá', {
+                    dangerouslyUseHTMLString: true,
+                    confirmButtonText: 'Đồng ý xoá',
+                    cancelButtonText: 'Không xoá',
+                    type: 'warning',
+                }
+            )
+            .then(() => {
+                ElMessage({
+                    dangerouslyUseHTMLString: true,
+                    type: 'info',
+                    message: `Đã có lỗi xảy ra khi xoá ${moduleName} <strong class="text-primary">${item.id}</strong>. embedded link đã được sử dụng nên không thể xoá`,
+                });
+            })
+            .catch(() => {
+                ElMessage({
+                    dangerouslyUseHTMLString: true,
+                    type: 'info',
+                    message: `Đã huỷ bỏ lệnh xoá ${moduleName} <strong class="text-primary">${item.id}</strong>`,
+                })
+            });
         };
 
         const route = useRoute();
--- src/scripts/fileUpload/add.ts
+++ src/scripts/fileUpload/add.ts
@@ -1,26 +1,97 @@
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, defineAsyncComponent } from 'vue';
+import SkeletonBox from '@/components/SkeletonBox.vue';
 import { mapState } from 'pinia';
 import { ElMessage } from 'element-plus';
 import { useDataCategoryStore } from '@/stores/dataCategory';
 import { fileUploadApi } from '@/api/fileUploadApi';
+import { FileSelector, Dropzone, DialogButton } from 'vue3-file-selector';
+import { humanFileSize } from "@/helpers/ultilityFunctions";
 const appState = useDataCategoryStore();
 export default {
     props: ['viewSettings'],
     emits: ['onChangeView'],
+	components: {
+		FileSelector,
+		Dropzone,
+		DialogButton,
+	},
     setup(props: any, context: any) {
+		var controllerUpload = new AbortController();
+        const fileSelectorRef = ref<any>(null);
+		const files = ref([]);
+        const uploadProgress = ref(0);
+
         const isLoading = ref(false);
+        const currentOrganizationName = ref('');
         const itemModel = ref({
-            nameOfDS: '',
-            descOfDS: '',
+            organizationId: 0,
         }); 
+
+        const onOrganizationChanged = (item:any)=>{
+            currentOrganizationName.value = item.name;
+        };
+
+        const removeFile = () => {
+			try{
+				files.value = [];
+				if(fileSelectorRef && fileSelectorRef.value && fileSelectorRef.value.inputRef) {
+					fileSelectorRef.value.inputRef.value="";
+				}
+			}
+			catch(error){					
+				console.error('removeFile ERROR', error);
+			}
+		};
+		const onProgressUpload = (evt:any) =>{
+			try{
+				uploadProgress.value =  Math.round((((evt.progress)*100 + Number.EPSILON)) * 100) / 100;
+			}
+			catch(error){					
+				console.error('onProgressUpload ERROR', error);
+			}
+		};
+		const cancelUpload = () =>{
+			try{
+				uploadProgress.value = 0;
+				controllerUpload.abort();
+				controllerUpload = new AbortController();
+				removeFile();
+			}
+			catch(error){					
+				console.error('cancelUpload ERROR', error);
+			}
+		};
 
         const addItemSubmit = () => {
             isLoading.value = true;
             try {
-                const data = {
-                    name: itemModel.value.nameOfDS,
-                    description: itemModel.value.descOfDS,
-                };
+                const formData = new FormData();
+                    formData.append('file', files.value[0]);
+                    fileUploadApi.uploadFileData(itemModel.value.organizationId, formData, controllerUpload, onProgressUpload).then((response: any) => {
+                        console.log('uploadFileData response', response);
+                        isLoading.value = false;
+                        if(response.data){
+                            switch(response.data.status){
+                                case 200:		
+                                case 400:						
+                                    ElMessage({
+                                        message: 'Đã tải lên file thành công !',
+                                        type: 'success',
+                                    });
+                                    removeFile();
+                                    break;
+                            }
+                        }
+                    })
+                    .catch((response:any) =>{
+                        console.error('uploadFileData ERROR_1', response);
+                        if(response.status && response.status === 400){
+                            
+                        }
+                        else{
+                            ElMessage.error('Tải file lên không thành công. Đã có lỗi xảy ra !');
+                        }
+                    });
             } catch (err) {
                 console.log(err);
                 isLoading.value = false;
@@ -31,9 +102,9 @@ export default {
             if (props.viewSettings) {
                 const orgId =  appState.defaultOrganization.id;
                 const orgName =  appState.defaultOrganization.name;
+                currentOrganizationName.value = orgName;
                 itemModel.value = {
-                    nameOfDS: '',
-                    descOfDS: '',
+                    organizationId: orgId,
                 };
             }
         });
@@ -41,6 +112,12 @@ export default {
             isLoading,
             itemModel,
             addItemSubmit,
+            onOrganizationChanged,
+            humanFileSize,
+            removeFile,
+            cancelUpload,
+            fileSelectorRef,
+            files
         };
     },
     computed: {
