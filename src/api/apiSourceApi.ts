f37a135bdf51b106349a3f81a4e2b0097d74a058
Minh Dao <daoducminh1997@gmail.com>
2023-02-15 11:43:53 +0700

    Update group for api and file

--- src/api/apiSourceApi.ts
+++ src/api/apiSourceApi.ts
@@ -1,27 +1,36 @@
 import axios from 'axios';
 export const apiSourceApi = {
     dataItemList(params: any) {
-        const requestParams:any = {
+        const requestParams: any = {
             page: params.page,
             size: params.size,
         };
         let requestUrl = `/apisource`;
-        if ( params.organization_id && params.organization_id !== null && params.organization_id !== '' )
-        {
+        if (
+            params.organization_id &&
+            params.organization_id !== null &&
+            params.organization_id !== ''
+        ) {
             requestUrl = `/organization/${params.organization_id}/apisource`;
         }
         return axios.get(requestUrl, { params: requestParams });
     },
-    getItem(itemId:number){
+    listByOrganization(organizationId: number) {
+        return axios.get(`/organization/${organizationId}/apisource/all`);
+    },
+    getItem(itemId: number) {
         return axios.get(`/apisource/${itemId}`);
     },
-    addItem(organization_id:number, itemData:any){
-        return axios.post(`/organization/${organization_id}/apisource`, itemData);
+    addItem(organization_id: number, itemData: any) {
+        return axios.post(
+            `/organization/${organization_id}/apisource`,
+            itemData
+        );
     },
-    updateItem(itemId:number, itemData:any){
+    updateItem(itemId: number, itemData: any) {
         return axios.put(`/apisource/${itemId}`, itemData);
-    }, 
-    deleteItem(itemId:number) {
+    },
+    deleteItem(itemId: number) {
         return axios.delete(`/apisource/${itemId}`);
-    }
-}
+    },
+};
--- src/scripts/dataSourceGroup/fileUpload/list.ts
+++ src/scripts/dataSourceGroup/fileUpload/list.ts
@@ -1,16 +1,17 @@
-import { ref, onMounted, nextTick } from "vue";
-import { mapState } from "pinia";
-import { fileUploadApi } from "@/api/fileUploadApi";
-import { useDataCategoryStore } from "@/stores/dataCategory";
-import { useRoute, useRouter } from "vue-router";
-import usePaginationList from "@/scripts/_baseScripts/_usePaginationList";
+import { ref, onMounted, nextTick } from 'vue';
+import { mapState } from 'pinia';
+import { fileUploadApi } from '@/api/fileUploadApi';
+import { useDataCategoryStore } from '@/stores/dataCategory';
+import { useRoute, useRouter } from 'vue-router';
+import usePaginationList from '@/scripts/_baseScripts/_usePaginationList';
+import { organizationApi } from '@/api/organizationApi';
 export default {
-    props: ["viewSettings"],
-    emits: ["onChangeView"],
+    props: ['viewSettings'],
+    emits: ['onChangeView'],
     setup() {
-        const moduleName = "File Upload";
+        const moduleName = 'File Upload';
         const filterDataInfo = ref({
-            organization_id: "",
+            organization_id: '',
         });
         const {
             isLoading,
@@ -23,7 +24,9 @@ export default {
             filterDataFn,
             deleteElement,
         } = usePaginationList(fileUploadApi.dataItemList, filterDataInfo);
- 
+
+        const groupElements: { id: any; organization: any; files: any }[] = [];
+
         const deleteItem = (item: any) => {
             deleteElement(
                 fileUploadApi.deleteItem(item.id),
@@ -32,23 +35,47 @@ export default {
             );
         };
 
-        const getDownloadUrl = (item:any) => {
-            return `${import.meta.env.VITE_API_URL}/organization/${item.organization.id}/file/${item.file_name}`;
+        const getDownloadUrl = (item: any) => {
+            return `${import.meta.env.VITE_API_URL}/organization/${
+                item.organization.id
+            }/file/${item.file_name}`;
+        };
+
+        const getGroup = async () => {
+            const or = await organizationApi.organizationListAll();
+            const organizations = or.data;
+
+            for (const organization of organizations) {
+                const r = await fileUploadApi.listByOrganization(
+                    organization.id
+                );
+
+                const files = r.data;
+
+                if (files.length > 0) {
+                    groupElements.push({
+                        id: organization.id,
+                        organization,
+                        files,
+                    });
+                }
+            }
         };
 
         const route = useRoute();
         const router = useRouter();
-        onMounted(() => {
-            if (route.query.organization) {
-                const organizationId: any = route.query.organization || "";
-                if (organizationId && organizationId !== "") {
-                    filterData.value.organization_id = parseInt(organizationId);
-                    const query = Object.assign({}, route.query);
-                    delete query.color;
-                    router.replace({ query: {} });
-                }
-            }
-            getListData(1);
+        onMounted(async () => {
+            // if (route.query.organization) {
+            //     const organizationId: any = route.query.organization || '';
+            //     if (organizationId && organizationId !== '') {
+            //         filterData.value.organization_id = parseInt(organizationId);
+            //         const query = Object.assign({}, route.query);
+            //         delete query.color;
+            //         router.replace({ query: {} });
+            //     }
+            // }
+            // getListData(1);
+            await getGroup();
         });
         return {
             isLoading,
@@ -61,11 +88,10 @@ export default {
             filterDataDebounceFn,
             deleteItem,
             getDownloadUrl,
+            groupElements,
         };
     },
     computed: {
-        ...mapState(useDataCategoryStore, [
-            "organization",
-        ]),
+        ...mapState(useDataCategoryStore, ['organization']),
     },
 };
