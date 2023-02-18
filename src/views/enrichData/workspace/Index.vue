c53158054294453ff0bb637ff077693c28e41246
duyhh <duyhh@irender.vn>
2023-02-18 15:00:10 +0700

    updtae

--- src/views/enrichData/workspace/Index.vue
+++ src/views/enrichData/workspace/Index.vue
@@ -41,36 +41,43 @@ import type { Share } from '@element-plus/icons-vue';
                         </div>
                         <div class="mt-3">
                             <div class="row row-eq-height">
-                                <div class="col-12 col-md-3 mb-3" v-for="item in 20" :key="item">
+                                <div class="col-12 col-md-3 mb-3 animate__animated animate__zoomIn" v-for="item in 20" :key="item">
                                     <el-card :body-style="{ padding: '0.8rem 0.8rem' }"
-                                        class="cursor-pointer">
+                                        class="cursor-pointer position-relative workspace-item-wrapper">
                                         <div>
-                                            <div><h5><span>Dữ liệu {{item}} Tỉnh Tuyên Quang</span></h5></div>
+                                            <div>
+                                                <h5>
+                                                    <i class="fas fa-rocket text-lightblue mr-2"></i><span>Dữ liệu {{item}} Tỉnh Tuyên Quang</span>
+                                                </h5>
+                                            </div>
                                             <div class="d-flex align-items-center" style="font-size: 90%;">
                                                 <el-icon class="text-primary"><Clock /></el-icon>
                                                 <span class="ml-2 text-muted">Chỉnh sửa lần cuối 1 giờ trước</span>
                                             </div>
-                                            <div class="mt-3 text-nowrap">
-                                                <el-button>
-                                                    <el-icon class="text-primary" :size="20" style="vertical-align: middle">
-                                                        <InfoFilled />
-                                                    </el-icon>
-                                                    <span class="ml-2 text-muted">Chi tiết</span>
-                                                </el-button>
+                                            <div>&nbsp;</div>
+                                            <div class="text-nowrap position-absolute item-actions d-flex align-items-center">
+                                                <div class="text-center w-100">
+                                                    <el-button>
+                                                        <el-icon class="text-primary" :size="20" style="vertical-align: middle">
+                                                            <InfoFilled />
+                                                        </el-icon>
+                                                        <span class="ml-2 text-muted">Chi tiết</span>
+                                                    </el-button>
 
-                                                <el-button>
-                                                    <el-icon class="text-primary" :size="20" style="vertical-align: middle">
-                                                        <Share />
-                                                    </el-icon>
-                                                    <span class="ml-2 text-muted">Chia sẻ</span>
-                                                </el-button>
+                                                    <el-button>
+                                                        <el-icon class="text-primary" :size="20" style="vertical-align: middle">
+                                                            <Share />
+                                                        </el-icon>
+                                                        <span class="ml-2 text-muted">Chia sẻ</span>
+                                                    </el-button>
 
-                                                <el-button type="danger">
-                                                    <el-icon :size="20" style="vertical-align: middle">
-                                                        <Delete />
-                                                    </el-icon>
-                                                    <span class="ml-2">Xoá</span>
-                                                </el-button>
+                                                    <el-button type="danger">
+                                                        <el-icon :size="20" style="vertical-align: middle">
+                                                            <Delete />
+                                                        </el-icon>
+                                                        <span class="ml-2">Xoá</span>
+                                                    </el-button>
+                                                </div>
                                             </div>
                                         </div>
                                     </el-card>
@@ -94,7 +101,36 @@ import type { Share } from '@element-plus/icons-vue';
                         </template>
                     </no-data>
                 </div>
+            </div>            
+            <div class="d-flex justify-content-end w-100">
+                <el-pagination class="self-align-end" background layout="prev, pager, next" :total="1000" />
             </div>
         </div>
     </el-card>
 </template>
+<style lang="scss" scoped>
+.workspace-item-wrapper{
+    cursor: pointer;
+    .item-actions{
+        right: -0.8rem;
+        top: -0.8rem;
+        left: -0.8rem;
+        bottom: -0.8rem;
+        background-color: rgba(136, 184, 255, 0.95);
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+        opacity: 1; 
+        transform: scale(0);
+    }
+
+    &:hover {
+        .item-actions{
+            transition: all 0.3s;
+            transition-delay: 0.1s;
+            opacity: 1;
+            transform: none;
+        }
+    }
+}
+</style>
