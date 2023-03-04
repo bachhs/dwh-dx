9387cadf2fd3ead20152a742754825670ec140de
JusteenDH <duyhh@irender.vn>
2023-03-04 08:01:42 +0700

    updtae

--- src/scripts/baseTools/categoriesManager/district/add.ts
+++ src/scripts/baseTools/categoriesManager/district/add.ts
@@ -1,107 +1,152 @@
-import { ref, onMounted, watch } from 'vue';
+import { ref, reactive, onMounted, defineAsyncComponent } from 'vue';
 import { mapState } from 'pinia';
 import { ElMessage } from 'element-plus';
+import type { FormInstance, FormRules } from 'element-plus';
 import { useDataCategoryStore } from '@/stores/dataCategory';
-import { fileEmbedLinkApi } from '@/api/fileEmbedLinkApi';
+import { districtApi } from '@/api/baseTools/districtApi';
 import moment from 'moment';
-const appState = useDataCategoryStore();
-//const organizationList = appState.organization;
-const startOfDay = moment().startOf('day');
-const endOfDay = moment().add(1, 'months').startOf('day');
+import SkeletonBox from '@/components/SkeletonBox.vue';
 export default {
     props: ['viewSettings'],
     emits: ['onChangeView'],
+    components: {
+        EditCategory: defineAsyncComponent({
+            loader: () =>
+                import(
+                    '@/views/baseTools/categoriesManager/_crudComponents/EditCategory.vue'
+                ),
+            loadingComponent: SkeletonBox,
+        }),
+    },
     setup(props: any, context: any) {
+        let isEditMode = false;
         const isLoading = ref(false);
-        const currentOrganizationName = ref('');
-        const validDateModel = ref([startOfDay.format("YYYY-MM-DD HH:mm:ss"), endOfDay.format("YYYY-MM-DD HH:mm:ss")]);
-        const itemModel = ref({
-            organizationId: 0,
-            validFrom: moment(validDateModel.value[0]).toDate(),
-            validTo: moment(validDateModel.value[1]).toDate(),
-        }); 
+        const editFields = ref([
+            { key: 'name', label: 'Tên Tỉnh/Thành phố' },
+            { key: 'full_name', label: 'Tên Tỉnh/Thành phố dạng đầy đủ' },
+        ]);
+        const itemModel = ref<any>({
+            countryId: 1,
+        });
 
-        const onOrganizationChanged = (item:any)=>{
-            currentOrganizationName.value = item.name;
+        const ruleFormRef = ref<FormInstance>();
+        const rules = reactive<FormRules>({
+            name: [
+                {
+                    required: true,
+                    message: 'Vui lòng không bỏ trống..',
+                    trigger: 'blur',
+                },
+            ],
+            full_name: [
+                {
+                    required: true,
+                    message: 'Vui lòng không bỏ trống..',
+                    trigger: 'blur',
+                },
+            ],
+        });
+
+        const createNewItem = (data: any) => {
+            districtApi
+                .addItem(data)
+                .then((response: any) => {
+                    if (response.data) {
+                        ElMessage({
+                            message: 'Thêm mới thành công',
+                            type: 'success',
+                        });
+                        context.emit('onChangeView', {
+                            viewName: 'ListData',
+                            data: null,
+                        });
+                    } else {
+                        ElMessage.error(`Oops, ${response.data.message}`);
+                    }
+                    isLoading.value = false;
+                })
+                .catch((error) => {
+                    console.error(error);
+                    isLoading.value = false;
+                });
         };
 
-        const addItemSubmit = () => {
-            isLoading.value = true;
-            try {
-                const data = {
-                    organization_id: itemModel.value.organizationId,
-                    valid_from: itemModel.value.validFrom,
-                    valid_to: itemModel.value.validTo,
-                };
-                fileEmbedLinkApi
-                    .addItem(data)
-                    .then((response: any) => {
-                        if (response.data) {
-                            ElMessage({
-                                message: 'Thêm mới nguồn dữ liệu thành công',
-                                type: 'success',
-                            });
-                            context.emit('onChangeView', {
-                                viewName: 'ListData',
-                                data: null,
-                            });
-                        } else {
-                            ElMessage.error(`Oops, ${response.data.message}`);
-                        }
-                        isLoading.value = false;
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                        isLoading.value = false;
-                    });
-            } catch (err) {
-                console.log(err);
-                isLoading.value = false;
-            }
-        }; 
+        const modifyItem = (itemId: number, data: any) => {
+            districtApi
+                .updateItem(itemId, data)
+                .then((response: any) => {
+                    if (response.data) {
+                        ElMessage({
+                            message: 'Cập nhật thành công',
+                            type: 'success',
+                        });
+                        context.emit('onChangeView', {
+                            viewName: 'ListData',
+                            data: null,
+                        });
+                    } else {
+                        ElMessage.error(`Oops, ${response.data.message}`);
+                    }
+                    isLoading.value = false;
+                })
+                .catch((error) => {
+                    console.error(error);
+                    isLoading.value = false;
+                });
+        };
 
-        watch(() => validDateModel.value, () => {
-            itemModel.value.validFrom = moment(validDateModel.value[0]).toDate();
-            itemModel.value.validTo = moment(validDateModel.value[1]).toDate();
-        });
+        const submitItemSubmit = async () => {
+            if (!ruleFormRef.value || !ruleFormRef.value) return;
+            await ruleFormRef.value.validate((valid, fields) => {
+                if (valid) {
+                    isLoading.value = true;
+                    try {
+                        const data = {
+                            ...itemModel.value,
+                        };
+                        if (!isEditMode) createNewItem(data);
+                        else modifyItem(itemModel.value.id, data);
+                    } catch (err) {
+                        console.log(err);
+                        isLoading.value = false;
+                    }
+                } else {
+                    console.log('error submit!', fields);
+                }
+            });
+        };
 
         onMounted(() => {
             if (props.viewSettings) {
-                if(props.viewSettings.dataItem === null){
-                    currentOrganizationName.value = appState.defaultOrganization.name;
+                if (props.viewSettings.dataItem === null) {
+                    isEditMode = false;
                     itemModel.value = {
-                        organizationId: appState.defaultOrganization.id,
-                        validFrom: moment(validDateModel.value[0]).toDate(),
-                        validTo: moment(validDateModel.value[1]).toDate(),
+                        countryId: 1,
                     };
-                }
-                else{
-                    let fileEmbedItem = props.viewSettings.dataItem;
-                    currentOrganizationName.value = fileEmbedItem.organization.name;
-                    let validFrom = moment(fileEmbedItem.valid_from);
-                    let validTo = moment(fileEmbedItem.valid_to);
-                    validDateModel.value = [validFrom.format("YYYY-MM-DD HH:mm:ss"), validTo.format("YYYY-MM-DD HH:mm:ss")];
+                } else {
+                    isEditMode = true;
+                    const editItem = props.viewSettings.dataItem;
                     itemModel.value = {
-                        organizationId: fileEmbedItem.organization.id,
-                        validFrom: moment(validDateModel.value[0]).toDate(),
-                        validTo: moment(validDateModel.value[1]).toDate(),
+                        countryId: 1,
+                        id: editItem.id,
                     };
+                    editFields.value.forEach((xField) => {
+                        itemModel.value[xField.key] = editItem[xField.key];
+                    });
                 }
             }
         });
         return {
             isLoading,
-            currentOrganizationName,
-            validDateModel,
+            editFields,
+            ruleFormRef,
+            rules,
             itemModel,
-            addItemSubmit,
-            onOrganizationChanged,
+            submitItemSubmit,
             moment,
         };
     },
     computed: {
-        ...mapState(useDataCategoryStore, [
-            'organization',
-        ]),
+        ...mapState(useDataCategoryStore, ['organization']),
     },
 };
